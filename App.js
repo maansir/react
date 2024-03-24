87e558ea94bbdedd75a40e45509083464a3e0263
maansir <m.farman4k@gmail.com>
2024-03-24 17:06:19 +0530

    First day code 01

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement('h1',
+{id:'heading', className:'headingcolor'},
+React.createElement('span',
+{id:'headiqng'},
+'Hello I am Mohd Farman from India.'));
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);
